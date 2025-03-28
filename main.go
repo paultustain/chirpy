79e0a5abeb1fd6fd3d4f23fd2156e789823c6598
paultustain <p.tustain@hotmail.co.uk>
2025-03-28 06:32:20 +0000

    Seperated functions to files for tidiness.

--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"net/http"
 	"sync/atomic"
 )
@@ -10,61 +9,23 @@ type apiConfig struct {
 	fileserverHits atomic.Int32
 }
 
-func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
-
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		cfg.fileserverHits.Add(1)
-		next.ServeHTTP(w, r)
-	})
-}
-
-func (cfg *apiConfig) metricsHandler() string {
-	return fmt.Sprintf("Hits: %v", cfg.fileserverHits.Load())
-}
-
-func (cfg *apiConfig) resetHandler() string {
-	cfg.fileserverHits.Store(0)
-	return "Reset fileserverHits"
-}
-
 func main() {
-	fs := http.FileServer(http.Dir("."))
-	mux := http.NewServeMux()
-	server := http.Server{
-		Handler: mux,
-		Addr:    ":8080",
-	}
+	const ROOTDIR = "."
 
 	apiCfg := apiConfig{}
 
-	mux.Handle("/app/", apiCfg.middlewareMetricsInc(http.StripPrefix("/app/", fs)))
+	mux := http.NewServeMux()
+	fs := http.FileServer(http.Dir(ROOTDIR))
+	mux.Handle("/app/", apiCfg.middlewareMetricsInc(http.StripPrefix("/app", fs)))
 
-	mux.HandleFunc(
-		"/healthz",
-		func(w http.ResponseWriter, r *http.Request) {
-			w.Header().Set("Content-Type", "text/plain; charset=utf-8")
-			w.WriteHeader(http.StatusOK)
-			w.Write([]byte("OK"))
-		},
-	)
+	mux.HandleFunc("GET /api/healthz", handlerReadiness)
+	mux.HandleFunc("GET /admin/metrics", apiCfg.handlerMetrics)
+	mux.HandleFunc("POST /admin/reset", apiCfg.handlerReset)
 
-	mux.HandleFunc(
-		"/metrics",
-		func(w http.ResponseWriter, r *http.Request) {
-			w.Header().Set("Content-Type", "text/plain; charset=utf-8")
-			w.WriteHeader(http.StatusOK)
-			w.Write([]byte(apiCfg.metricsHandler()))
-		},
-	)
-
-	mux.HandleFunc(
-		"/reset",
-		func(w http.ResponseWriter, r *http.Request) {
-			w.Header().Set("Content-Type", "text/plain; charset=utf-8")
-			w.WriteHeader(http.StatusOK)
-			w.Write([]byte(apiCfg.resetHandler()))
-		},
-	)
+	server := &http.Server{
+		Handler: mux,
+		Addr:    ":8080",
+	}
 
 	server.ListenAndServe()
 
